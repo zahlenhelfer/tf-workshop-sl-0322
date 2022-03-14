7f6fee0a9aeceb88528e5629242bfc8993e42176
Marcus Ross <marcus.ross@zahlenhelfer.de>
2022-03-14 12:39:48 +0100

    erstes einfaches Beispiel

---
+++ main.tf
@@ -0,0 +1,10 @@
+provider "aws" {
+  region = "us-west-2"
+}
+resource "aws_instance" "app_server" {
+  ami           = "ami-830c94e3"
+  instance_type = "t2.micro"
+tags = {
+    Name = "ExampleAppServerInstance"
+  }
+}
