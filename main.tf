aa3428578498689207cf00bf58c73a26712303c6
Marcus Ross <marcus.ross@zahlenhelfer.de>
2022-03-14 13:58:34 +0100

    versionierung des Providers hinzugefügt

--- main.tf
+++ main.tf
@@ -1,10 +1,21 @@
+terraform {
+  required_providers {
+    aws = {
+      source  = "hashicorp/aws"
+      version = ">=3.71"
+    }
+  }
+  required_version = ">=1.0"
+}
+
 provider "aws" {
   region = "us-west-2"
 }
+
 resource "aws_instance" "app_server" {
   ami           = "ami-830c94e3"
   instance_type = "t2.micro"
-tags = {
+  tags = {
     Name = "ExampleAppServerInstance"
   }
 }
