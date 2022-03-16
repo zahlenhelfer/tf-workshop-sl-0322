b2b6a17b007f192389d3b93105d66e4259cd72ee
Marcus Ross <marcus.ross@zahlenhelfer.de>
2022-03-16 17:00:41 +0100

    vpc-modul beispiel und slides hinzugefuegt

--- appserver.tf
+++ appserver.tf
@@ -33,6 +33,15 @@ resource "aws_security_group_rule" "allow-webserver-access" {
   security_group_id = aws_security_group.web-access.id
 }
 
+resource "aws_security_group_rule" "allow-ssh-access" {
+  type              = "ingress"
+  from_port         = 22
+  to_port           = 22
+  protocol          = "tcp"
+  cidr_blocks       = ["0.0.0.0/0"]
+  security_group_id = aws_security_group.web-access.id
+}
+
 resource "aws_security_group_rule" "allow-webserver-access-egress" {
   type              = "egress"
   from_port         = 0
